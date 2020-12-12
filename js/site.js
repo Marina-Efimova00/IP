3b120f96620980ed1062bd6fe1316d12110d8367
Марина Ефимова <marina.efimova.00@bk.ru>
2020-12-13 01:34:58 +0400

    laba5

--- js/site.js
+++ js/site.js
@@ -8,8 +8,8 @@ form.addEventListener('submit', (event) => {
     if (!checkPhone(values[2])) return showMessage('error', 'Неверно введен номер телефона');
     if (!checkIfNotNull(values[3])) return showMessage('error', 'Заполните поле Город');
     if (!checkIfNotNull(values[4])) return showMessage('error', 'Заполните поле Адрес');
-   // if (!checkRadio(values[5])) return showMessage('error', 'Выберете способ доставки');
-   // if (!checkRadio(values[6])) return showMessage('error', 'Выберете способ оплаты');
+    if (!checkRadioOne(values[5])) return showMessage('error', 'Выберете способ доставки');
+    if (!checkRadioTwo(values[6])) return showMessage('error', 'Выберете способ оплаты');
     if (!checkNumCard(values[7])) return showMessage('error', 'Заполните поле Номер карты');
     showMessage('success', 'Оплачено');
 });
@@ -20,18 +20,38 @@ function checkPhone(number) {
     var reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
     return number != "" && reg.test(number);
 }
-function checkRadio(radio) {
-    var reg = !('[type="radio"]:checked');
-    return radio != "" && reg.test(radio);
+function checkRadioOne(rad) {
+    
+    var inp = document.getElementsByName('rad');
+    for (var i = 0; i < inp.length; i++) {
+        var radio = inp[i];
+        if (radio.type == "radio" && radio.checked ) {
+           return true
+        }
+    }
+    return false;
+}
+function checkRadioTwo(rad2) {
+    
+    var inp = document.getElementsByName('rad2');
+    for (var i = 0; i < inp.length; i++) {
+        var radio = inp[i];
+        if (radio.type == "radio" && radio.checked ) {
+           return true;
+        }
+        
+    }
+    return false;
+}
+function checkNumCard(card) {
+    var reg = /^\d{16}$/;
+    return card != "" && reg.test(card);
+    
+    return card !== '';
 }
 function checkEmail(email) {
     var reg = /^([A-Za-z0-9-.])+@([A-Za-z0-9-.])+.([A-Za-z]{2,4})$/;
     return email != "" && reg.test(email);
-}
-function checkNumCard(str) {
-    if(!($('input[name = gridRadios1]:checked').length > 0) && str != ""){
-        return str !== '';
-    }
 }
 function showMessage(type, text) {
     Swal.fire({
