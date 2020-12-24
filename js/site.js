5ae4d81bf2cbcd964f93e7814ea75fd4d1a32a6d
Марина Ефимова <marina.efimova.00@bk.ru>
2020-12-24 22:19:02 +0400

    fix

--- js/site.js
+++ js/site.js
@@ -1,20 +1,22 @@
-const form = document.forms[1];
+const form = document.forms['formOrder'];
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const values = [...new FormData(form).values()];
-    console.log(values);
-    if (!checkIfNotNull(values[0])) return showMessage('error', 'Заполните поле ФИО');
-    if (!checkEmail(values[1])) return showMessage('error', 'Неверно введен Email');
-    if (!checkPhone(values[2])) return showMessage('error', 'Неверно введен номер телефона');
-    if (!checkIfNotNull(values[3])) return showMessage('error', 'Заполните поле Город');
-    if (!checkIfNotNull(values[4])) return showMessage('error', 'Заполните поле Адрес');
-    if (!checkRadioOne(values[5])) return showMessage('error', 'Выберете способ доставки');
-    if (!checkRadioTwo(values[6])) return showMessage('error', 'Выберете способ оплаты');
-    if (!checkNumCard(values[7])) return showMessage('error', 'Заполните поле Номер карты');
+    const formData = new FormData(form);
+    console.log(...formData.values());
+
+    if (!checkIfNotNull(formData.get('fio'))) return showMessage('error', 'Заполните поле ФИО');
+    if (!checkEmail(formData.get('email'))) return showMessage('error', 'Неверно введен Email');
+    if (!checkPhone(formData.get('num'))) return showMessage('error', 'Неверно введен номер телефона');
+    if (!checkIfNotNull(formData.get('city'))) return showMessage('error', 'Заполните поле Город');
+    if (!checkIfNotNull(formData.get('adress'))) return showMessage('error', 'Заполните поле Адрес');
+    if (!checkRadioOne(formData.get('rad'))) return showMessage('error', 'Выберете способ доставки');
+    if (!checkRadioTwo(formData.get('radTwo'))) return showMessage('error', 'Выберете способ оплаты');
+    if (!checkNumCard(formData.get('card'))) return showMessage('error', 'Введите номер карты состоящий из 16 символов');
     showMessage('success', 'Оплачено');
 });
-function checkIfNotNull(str) {
-    return str !== '';
+
+function checkIfNotNull(fio) {
+    return fio !== '';
 }
 function checkPhone(number) {
     var reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
@@ -31,9 +33,9 @@ function checkRadioOne(rad) {
     }
     return false;
 }
-function checkRadioTwo(rad2) {
+function checkRadioTwo(radTwo) {
     
-    var inp = document.getElementsByName('rad2');
+    var inp = document.getElementsByName('radTwo');
     for (var i = 0; i < inp.length; i++) {
         var radio = inp[i];
         if (radio.type == "radio" && radio.checked ) {
